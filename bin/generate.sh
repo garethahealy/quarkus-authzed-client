b7d5f1b3c46796334801749137092902a530be20
Gareth Healy <garethahealy@gmail.com>
2024-04-18 16:58:07 +0100

    Update generate.sh

--- bin/generate.sh
+++ bin/generate.sh
@@ -15,19 +15,18 @@ echo "Updating proto files using $PROTO_SHA"
 rm -rf $PROTO_DIR
 mkdir -p $PROTO_DIR
 
-docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/envoyproxy/protoc-gen-validate -o $PROTO_DIR
-docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/grpc-ecosystem/grpc-gateway -o $PROTO_DIR
-docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/authzed/api:${PROTO_SHA} -o $PROTO_DIR
-
-ls -lrt
-find . -name permission_service.proto
+docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/envoyproxy/protoc-gen-validate -o "/workspace/grpc/generator/src/main/proto"
+docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/grpc-ecosystem/grpc-gateway -o "/workspace/grpc/generator/src/main/proto"
+docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf export buf.build/authzed/api:${PROTO_SHA} -o "/workspace/grpc/generator/src/main/proto"
 
 echo "Generating gRPC client"
 mvn clean package -Pgenerate -pl :quarkus-authzed-grpc-generator
+
 echo "Removing existing gRPC client files"
 rm -r $TARGET_DIR/com
 rm -r $TARGET_DIR/grpc
 rm -r $TARGET_DIR/io/envoyproxy
+
 # We don't delete everything cause we need to retain io/quarkiverse for BearerToken
 echo "Copying gRPC client"
 cp -r $GENERATED_DIR/* $TARGET_DIR
